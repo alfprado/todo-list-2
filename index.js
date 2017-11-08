19d84a05cddf88098999ba7300ae6f1192077995
Andre Luiz Fernandes do Prado <andre@hostname.yourdomain.com>
2017-11-08 00:13:48 -0200

    Add code from example - HTML, CSS and JS.
    /views/index.html
    /public/app.css
    /public/app.js
    
    fix index.js, mongoose deprecation warning.

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var todoRoutes = require('./routes/todos');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-	res.send("HI ROOT!");
+	res.sendFile("index.html");
 });
-
 
 app.use('/api/todos', todoRoutes);
 
